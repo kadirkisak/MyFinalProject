fb149f040fcfa93a1727fabe40e4c5e8d4b205a8
Kadir Kısak <kadirkisak@gmail.com>
2021-03-14 21:39:49 +0300

    13 Mart Ders 2

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebAPI.Controllers
@@ -21,10 +22,12 @@ namespace WebAPI.Controllers
         {
             _productService = productService;
         }
-
+        
         [HttpGet("getall")]
         public IActionResult GetAll()
         {
+            Thread.Sleep(2500
+            );
             var result = _productService.GetAll();
             if (result.Success)
             {
@@ -37,6 +40,16 @@ namespace WebAPI.Controllers
         public IActionResult GetById(int id)
         {
             var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int categoryId)
+        {
+            var result = _productService.GetAllByCategoryId(categoryId);
             if (result.Success)
             {
                 return Ok(result);
